f0598a50eaf3dceae539ea45149ad006a4169f94
brunovittoria <brunogun1999@gmail.com>
2025-04-01 08:22:36 -0300

    feat: created export's for types and updated tsconfig

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+// API Base Types
+export * from './api/base'
+
+// API Request Types
+export * from './api/requests/auth'
+export * from './api/requests/exercise'
+export * from './api/requests/subscription'
+export * from './api/requests/user'
+export * from './api/requests/workout'
+
+// API Response Types
+export * from './api/responses/auth'
+export * from './api/responses/exercise'
+export * from './api/responses/subscription'
+export * from './api/responses/user'
+export * from './api/responses/workout'
+
+// Model Types
+export * from './models/exercise'
+export * from './models/subscription'
+export * from './models/user'
+export * from './models/workout'
+
+// Components Types
+
+// Store Types
