9b2b529c6e5868d1dd87d493083bc63da399b67f
brunovittoria <brunogun1999@gmail.com>
2025-04-11 08:55:46 -0300

    refactor: update import for API endpoints in user service

--- src/services/modules/user.ts
+++ src/services/modules/user.ts
@@ -6,10 +6,9 @@ import type {
 } from '@/types/api/responses/user'
 import type { UpdateUserRequest } from '@/types/api/requests/user'
 import { User } from '@/types/models/user'
+import { API_ENDPOINTS } from '@/types/api/base'
 
-import { ApiEndpoints } from '../../types'
-
-const { PROFILE } = ApiEndpoints
+const { PROFILE } = API_ENDPOINTS
 
 export const userService = {
   async getUserProfile(): Promise<User> {
